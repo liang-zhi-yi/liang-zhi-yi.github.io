26b10ef1fef5d7608cde0cd22d76b834db64d990
liang-zhi-yi <liang-zhi-yi@example.com>
2026-05-28 09:12:59 +0800

    fix toc alignment and smooth home arrows

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,9 +1,7 @@
-const zhCnDateFormatter = new Intl.DateTimeFormat('zh-CN', {
-  year: 'numeric',
-  month: '2-digit',
-  day: '2-digit'
-});
-
 export function formatDate(date: Date) {
-  return zhCnDateFormatter.format(date);
+  return date.toLocaleDateString('zh-CN', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit'
+  });
 }
